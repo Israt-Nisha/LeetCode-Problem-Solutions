3e8379fa7d03da7377f885f37f1bcc38123202f7
Israt-Nisha <isratnisha1999@gmail.com>
2026-07-16 19:36:57 +0600

    feat: solve LeetCode #21 MergeTwoList

--- linkList.js
+++ linkList.js
@@ -70,3 +70,32 @@ const reversed = reverseList(head);
 
 console.log("Reversed:");
 printLinkedList(reversed);
+
+// Problem 21 (Merge Two Sorted Lists),
+
+var mergeTwoLists = function(list1, list2) {
+    let dummy = new ListNode(-1);
+    let tail = dummy;
+
+    while (list1 !== null && list2 !== null) {
+        if (list1.val <= list2.val) {
+            tail.next = list1;
+            list1 = list1.next;
+        } else {
+            tail.next = list2;
+            list2 = list2.next;
+        }
+
+        tail = tail.next;
+    }
+
+    if (list1 !== null) {
+        tail.next = list1;
+    }
+
+    if (list2 !== null) {
+        tail.next = list2;
+    }
+
+    return dummy.next;
+};
